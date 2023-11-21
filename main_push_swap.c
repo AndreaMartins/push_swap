79a3bc5bcad9ca7cc7c2fcc85e695f75a637ca27
Andrea Martins <andmart2@car9s6.42barcelona.com>
2023-11-21 14:22:42 +0100

    variables in list 21.11.23

--- main_push_swap.c
+++ main_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: andmart2 <andmart2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 11:12:32 by andmart2          #+#    #+#             */
-/*   Updated: 2023/11/17 12:38:10 by andmart2         ###   ########.fr       */
+/*   Updated: 2023/11/21 13:05:06 by andmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,48 @@ int	main(int ac, char **av)
 		ft_free(key_list);
 	ft_complexfree(key_list);
 	return (0);
+}
+
+int	ft_size_selector(int ac, t_list *key_list)
+{
+	/*check there aren't repetitive numbers*/
+	if (check_repeated_nums(key_list) == -1)
+		return(-1);
+	else
+	{
+		if(ac == 3 || ac == 4)
+			size_small(key_list);
+		if(ac < 7 &&  ac < 4)
+			size_medium(key_list);
+		if(ac < 102 && ac > 6)
+			size_big(key_list);
+		if(c > 101)
+			size_extra(key_list);
+	}
+	return(0);
+}	
+
+int av_to_init(t_list *key_list, int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	while (i != ac - 1)
+	{
+		if(check_is_num(av[i + 1] == -1))
+			return (-1);
+		key_list->stack_a[i] = ft_atoi(av[i+1], key_list);
+		if (key_list ->atoierror == 1)
+			return(-1);
+		key_list->stack_sort[i] = ft_atoi(av[i +1], key_list);
+		i++;
+	}
+	return(0);
+}
+
+
+int	init_data(t_list *key_list)
+{
+	key_list->stack_a = malloc(sizeof(int) * key_list -> ac_list);
+
 }
--- pushswap.h
+++ pushswap.h
@@ -6,7 +6,7 @@
 /*   By: andmart2 <andmart2@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 12:12:31 by andmart2          #+#    #+#             */
-/*   Updated: 2023/11/17 12:47:44 by andmart2         ###   ########.fr       */
+/*   Updated: 2023/11/21 14:16:12 by andmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,23 @@
 
 typedef struct s_list
 {
-	int	*stack_a;
+	int	*stack_a; 
 	int	*stack_b;
-	int	*stack_sort;
-	int	size_a;
-	int	size_b;
-	int	stack_index;
-	int	count;
-	int	error;
+	int	*stack_sort; //array for sorting purposes
+	int	size_a;//size stack a
+	int	size_b;//size stack b
+	int size_ip;//stack index position(related to position in the stack)
+	int size_lsp;//last size of stack position 
+	int ac_list;//number comand line arguments
+	int	percentage; //percentage used in algorithm
+	int control;//control variable
+	int cont;//control variable count or cantainer 
+	int error;
+	int atoierror;
+	int jatoi;//variable for atoi
+	int size_block; //size of block used in algorithm
+	int argmax;// max number command line
+	int arguments;//num of arguments used in algorithm 
 }	t_list;
 
 #endif
